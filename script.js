cbe1410fe42fb04b873f10697c86fbe7444a5961
Emmanuel Boluwatife Janet <88049758+Tifem@users.noreply.github.com>
2024-10-07 08:30:08 -0700

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-alert('Good Morning!')
+
+
+
+function calculate(num1,num2,operator){
+    if (operator === '+'){
+        return num1 + num2
+    }else if (operator === '-'){
+        return num1 - num2
+    }else if(operator === '/'){
+        return num1 / num2
+    }else if(operator === '*'){
+        return num1 * num2
+    }else if(operator === '%'){
+        return (num1 % num2)
+    }else{
+        return "Invalid Operator"
+    }
+}
+
+const num1 = 15;
+const num2 = 3;
+const operator = '/';
+
+const result = calculate(num1,num2,operator);
+console.log("Result: " + result.toFixed(2));
